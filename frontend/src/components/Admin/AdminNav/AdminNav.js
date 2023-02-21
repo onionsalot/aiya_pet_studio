093f825ebf88a733aab2d94e5b3a45bade4e8756
onionsalot <onionsinmypants@gmail.com>
2023-02-21 00:57:55 -0500

    redesigns admin page to include sidenav

---
+++ frontend/src/components/Admin/AdminNav/AdminNav.js
@@ -0,0 +1,37 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDatabase, faTruckFast } from '@fortawesome/free-solid-svg-icons'
+import NavLink from './NavLink'
+
+const AdminNav = () => {
+  const database = <FontAwesomeIcon icon={faDatabase} />
+  const truck = <FontAwesomeIcon icon={faTruckFast} />
+
+  return ( 
+    <div className="flex flex-col bg-blue-500 h-screen w-64 pl-2 pt-4">
+      <span className="text-admin_main text-2xl mb-3">Admin Pet Studio</span>
+      <hr className="w-40 content-center mb-3"/>
+      <div className="text-admin_link2">
+        <div>
+          <label className="text-admin_main text-lg">{truck} Quick Launch</label>
+          <ul className="list-none p-1 pl-3">
+            <li><NavLink url="/admin/dashboard" text="DashBoard"/></li>
+          </ul>
+        </div>
+        <br />
+        <div>
+          <label className="text-admin_main text-lg">{database} Object Management</label>
+          <ul className="list-none p-1 pl-3">
+            <li><NavLink url="/admin/dashboard" text="Users"/></li>
+            <li><NavLink url="/admin/dashboard" text="Products"/></li>
+            <li><NavLink url="/admin/dashboard" text="Tags"/></li>
+            <li><NavLink url="/admin/dashboard" text="Categories"/></li>
+            <li><NavLink url="/admin/dashboard" text="Invoices"/></li>
+            <li><NavLink url="/admin/dashboard" text="Reviews"/></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+ 
+export default AdminNav
--- frontend/src/components/Auth/Logout.js
+++ frontend/src/components/Auth/Logout.js
@@ -1,7 +1,10 @@
 import { useAuth } from "../../hooks/use-auth"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 
 const Logout = () => {
   const { signoutUserMutation } = useAuth()
+  const logoutIcon = <FontAwesomeIcon icon={faRightFromBracket} />
 
   const handleClick = async (e) => {
     e.preventDefault()
@@ -10,7 +13,7 @@ const Logout = () => {
 
   return (
     <div>
-      <input type="button" value="Logout" onClick={handleClick} />
+      {logoutIcon} <input type="button" value="Logout" onClick={handleClick} />
     </div>
   )
 }
