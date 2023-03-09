5f5be8dc1534bd9da77bb7d322c108cf9c046613
Angel Zhou <angel.zhou1016@gmail.com>
2023-03-09 17:06:22 -0500

    adds show all page for products

---
+++ frontend/src/pages/ProductsPage/ProductsPage.js
@@ -0,0 +1,48 @@
+import Table from "../../components/Admin/Table/Table"
+import { useProducts } from "../../hooks/product-hooks"
+import { useMemo } from "react"
+
+const ProductsPage = () => {
+    const products = useProducts()
+    const columns = useMemo(
+        () => [
+            {
+                Header: "Products",
+                columns: [
+                    {
+                        Header: "ID",
+                        accessor: "id",
+                    },
+                    {
+                        Header: "Name",
+                        accessor: "name",
+                    },
+                    {
+                        Header: "Price",
+                        accessor: "price",
+                    },
+                    {
+                        Header: "Description",
+                        accessor: "description",
+                    },
+                    {
+                        Header: "Created At",
+                        accessor: "createdAt",
+                    },
+                ],
+            },
+        ],
+        []
+    )
+
+    if (products.isError) return <h1>Something went wrong!</h1>
+    if (products.isLoading) return <h1>Loading...</h1>
+
+    return (
+        <div className="bg-white h-full overflow-y-scroll">
+            <Table columns={columns} data={products?.data?.data?.data?.tags} />
+        </div>
+    )
+}
+
+export default ProductsPage
