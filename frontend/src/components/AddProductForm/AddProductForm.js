71098619f085a93d8245d2f825e5cc58c34d64f2
eerian <eerian00@gmail.com>
2023-03-11 18:04:50 -0500

    fixed linting issue on AddProductForm

--- frontend/src/components/AddProductForm/AddProductForm.js
+++ frontend/src/components/AddProductForm/AddProductForm.js
@@ -1,27 +1,27 @@
-import { useMutate } from "../../hooks/use-mutate";
-import { useRef } from "react";
+import { useMutate } from "../../hooks/use-mutate"
+import { useRef } from "react"
 
 const AddProductForm = () => {
-  const formRef = useRef();
-  const { addProduct } = useMutate();
+  const formRef = useRef()
+  const { addProduct } = useMutate()
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    const formData = new FormData(formRef.current);
-    const data = Object.fromEntries(formData);
-    const input = { name: data.name, price: parseFloat(data.price) };
-    addProduct.mutate(input);
-    e.target.reset();
-  };
+    e.preventDefault()
+    const formData = new FormData(formRef.current)
+    const data = Object.fromEntries(formData)
+    const input = { name: data.name, price: parseFloat(data.price)}
+    addProduct.mutate(input)
+    e.target.reset()
+  }
 
   return (
     <>
       <form ref={formRef} onSubmit={handleSubmit}>
-        Name: <input type="name" name="name" required />
-        Price: <input type="number" name="price" required />
+        Name: <input type="name" name="name" required/>
+        Price: <input type="number" name="price" required/>
         <input type="submit" value="Submit" />
       </form>
     </>
-  );
-};
-export default AddProductForm;
+  )
+}
+export default AddProductForm
