1767632bf7cf4e7e62ee0850fa60aaf45315d982
Erion Pali <eerian00@gmail.com>
2023-03-10 22:18:52 -0500

    makes logout button clickable (#18)
    * makes logout button clickable
    
    * changed logout button styling to use tailwind

--- frontend/src/components/Auth/Logout.js
+++ frontend/src/components/Auth/Logout.js
@@ -1,21 +1,22 @@
-import { useAuth } from "../../hooks/use-auth"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { useAuth } from "../../hooks/use-auth";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 
 const Logout = () => {
-  const { signoutUserMutation } = useAuth()
-  const logoutIcon = <FontAwesomeIcon icon={faRightFromBracket} />
+  const { signoutUserMutation } = useAuth();
+  const logoutIcon = <FontAwesomeIcon icon={faRightFromBracket} />;
 
   const handleClick = async (e) => {
-    e.preventDefault()
-    signoutUserMutation.mutate()
-  }
+    e.preventDefault();
+    signoutUserMutation.mutate();
+  };
 
   return (
-    <div>
-      {logoutIcon} <input type="button" value="Logout" onClick={handleClick} />
+    <div class="mr-3 cursor-pointer" onClick={handleClick}>
+      {logoutIcon}
+      <input type="button" value="Logout" class="ml-1 cursor-pointer" />
     </div>
-  )
-}
+  );
+};
 
-export default Logout
+export default Logout;
