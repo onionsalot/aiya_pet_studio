3bc53f140630bb1e35ffd333c4dd66cfb0509ce7
Angel Zhou <109117779+angel-zh@users.noreply.github.com>
2023-03-15 16:37:54 -0400

    APS-34 Adds PaperTrail (#21)
    * adjusts description input box size
    
    * truncates long text in table and updates table width and icons
    
    * truncates long text in table and updates table width and icons
    
    * adds hover effect for rows
    
    * installs PaperTrail and adds it to product and user model to track changes
    
    * Removes modified file from pull request

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,4 +10,5 @@
 #  updated_at  :datetime         not null
 #
 class Product < ApplicationRecord
+    has_paper_trail
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -33,6 +33,7 @@
 #  phone_number           :string
 #
 class User < ApplicationRecord
+  has_paper_trail
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
