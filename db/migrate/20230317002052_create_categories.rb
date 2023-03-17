7ee784b9dfec32048cf94a92950a725e4cd2a5c4
Erion <eerian00@gmail.com>
2023-03-16 21:09:36 -0400

    adds default values to catagories model

--- db/migrate/20230317002052_create_categories.rb
+++ db/migrate/20230317002052_create_categories.rb
@@ -1,9 +1,9 @@
 class CreateCategories < ActiveRecord::Migration[7.0]
   def change
     create_table :categories do |t|
-      t.string :display
-      t.integer :order
-      t.string :link_to
+      t.string :display, default: ""
+      t.integer :order, default: 0
+      t.string :link_to, default: ""
 
       t.timestamps
     end
