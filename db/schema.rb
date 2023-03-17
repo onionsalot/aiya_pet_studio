3a6350d6d9514404e6749e2be3bf57990fc8c393
Angel Zhou <angel.zhou1016@gmail.com>
2023-03-17 17:37:17 -0400

    updates product model with additional fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_14_162357) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_17_212120) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_14_162357) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "etsy_listing_id"
+    t.integer "quantity"
   end
 
   create_table "tags", force: :cascade do |t|
