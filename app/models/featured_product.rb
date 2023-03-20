e38f9c37672a74bdccc4db9501d33a98b3032110
onionsalot <onionsinmypants@gmail.com>
2023-03-20 00:16:50 -0400

    Adds maximum record count to FeaturedProduct

--- app/models/featured_product.rb
+++ app/models/featured_product.rb
@@ -9,4 +9,15 @@
 #
 class FeaturedProduct < ApplicationRecord
   belongs_to :product, optional: true
+  validate :maximum_record_limit, on: :create
+
+  MAX_RECORDS = 4
+
+  private
+
+  def maximum_record_limit
+    if self.class.count >= MAX_RECORDS && new_record?
+      errors.add(:base, "The maximum number of records (#{MAX_RECORDS}) has been reached")
+    end
+  end
 end
