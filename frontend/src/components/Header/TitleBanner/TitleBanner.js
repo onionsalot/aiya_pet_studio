88424d771404912ba425464ff760a4fe12318db3
Angel Zhou <angel.zhou1016@gmail.com>
2023-03-20 17:32:05 -0400

    updates styles on search bar and hover effects

--- frontend/src/components/Header/TitleBanner/TitleBanner.js
+++ frontend/src/components/Header/TitleBanner/TitleBanner.js
@@ -47,19 +47,19 @@ const TitleBanner = () => {
         <Link to="/">Aiya Pet Studio</Link>
       </h1>
       <div className="flex text-gray-600">
-        <div className="flex border-2 rounded-md border-gray-600 mr-2">
-          <input type="text" className="bg-transparent"></input>
-          <button className="hover:text-indigo-400">{searchIcon}</button>
+        <div className="flex border-2 rounded-md border-gray-600 mr-2 px-1">
+          <input type="text" className="bg-transparent focus:outline-none"></input>
+          <button className="hover:text-indigo-400 ease-in duration-150">{searchIcon}</button>
         </div>
         <div className="group inline-block relative mx-2 mt-1 ">
           <button>
-            <span className="hover:text-indigo-400">{userIcon}{dropdownIcon}</span>
+            <span className="hover:text-indigo-400 ease-in duration-150">{userIcon}{dropdownIcon}</span>
           </button>
-          <ul className="absolute -left-8 hidden group-hover:block drop-shadow-lg">
+          <ul className="absolute -left-8 hidden group-hover:block drop-shadow-lg ease-in duration-300">
             {user ? authenticatedOptions : unauthenticatedOptions}
           </ul>
         </div>
-        <Link to="/app/cart" className="hover:text-indigo-400">{bagIcon}</Link>
+        <Link to="/app/cart" className="hover:text-indigo-400 ease-in duration-150">{bagIcon}</Link>
       </div>
     </div>
   )
