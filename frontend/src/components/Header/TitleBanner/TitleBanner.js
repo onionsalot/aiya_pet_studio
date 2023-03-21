76d589efa751abb96c79f2668e829b58c592bc88
Angel Zhou <angel.zhou1016@gmail.com>
2023-03-21 12:50:49 -0400

    adjusts user icon

--- frontend/src/components/Header/TitleBanner/TitleBanner.js
+++ frontend/src/components/Header/TitleBanner/TitleBanner.js
@@ -47,13 +47,13 @@ const TitleBanner = () => {
         <Link to="/">Aiya Pet Studio</Link>
       </h1>
       <div className="flex text-gray-600">
-        <div className="flex border-2 rounded-md border-gray-600 mr-2 px-1">
+        <div className="flex border-2 rounded-md border-gray-600 mr-2 px-1 hover:border-indigo-400 ease-in duration-150">
           <input type="text" className="bg-transparent focus:outline-none"></input>
           <button className="hover:text-indigo-400 ease-in duration-150">{searchIcon}</button>
         </div>
         <div className="group inline-block relative mx-2 mt-1 ">
-          <button>
-            <span className="hover:text-indigo-400 ease-in duration-150">{userIcon}{dropdownIcon}</span>
+          <button className="hover:text-indigo-400 ease-in duration-150">
+            <span>{userIcon}{dropdownIcon}</span>
           </button>
           <ul className="absolute -left-8 hidden group-hover:block drop-shadow-lg">
             {user ? authenticatedOptions : unauthenticatedOptions}
