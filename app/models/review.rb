5e20bc2f47040bd8c3c594ba70b8ec95ed70cd63
onionsalot <onionsinmypants@gmail.com>
2023-03-25 15:19:23 -0400

    Adds function to check who the reviewer is for a review

--- app/models/review.rb
+++ app/models/review.rb
@@ -16,4 +16,11 @@ class Review < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :product
   has_one :etsy_review, class_name: 'Etsy::EtsyReview', dependent: :nullify
+
+  def reviewer
+    return user.email if user.present?
+    return etsy_review.buyer_email if etsy_review.present? && etsy_review.buyer_email.present?
+
+    "anonymous"
+  end
 end
