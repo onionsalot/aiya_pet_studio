cd4985aeb01b5490e500b62dc0b2d98fb200a7a2
Angel Zhou <angel.zhou1016@gmail.com>
2023-03-26 22:16:29 -0400

    adjusts hover opacity

--- frontend/src/components/Header/TitleBanner/SearchBar.js
+++ frontend/src/components/Header/TitleBanner/SearchBar.js
@@ -4,9 +4,9 @@ const SearchBar = () => {
   const searchIcon = <BiSearch className="text-2xl" />
   
   return (
-    <div className="flex justify-between border-2 rounded-md border-gray-600 mx-2 px-1 hover:border-indigo-400 ease-in duration-150">
+    <div className="flex justify-between border-2 rounded-md border-gray-600 mx-2 px-1 hover:text-indigo-400/80 hover:border-indigo-400/80 ease-in duration-150">
       <input type="text" className="bg-transparent focus:outline-none w-full"></input>
-      <button className="hover:text-indigo-400 ease-in duration-150">{searchIcon}</button>
+      <button>{searchIcon}</button>
     </div>
   )
 }
