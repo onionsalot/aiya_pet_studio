febdb31c71a705628d51bb6833a5439d1f86163b
Angel Zhou <angel.zhou1016@gmail.com>
2023-03-28 23:02:58 -0400

    updates order to desc

--- app/graphql/queries/ten_newest_reviews.rb
+++ app/graphql/queries/ten_newest_reviews.rb
@@ -3,7 +3,7 @@ module Queries
     type [Types::ReviewType], null: false
     description "Query ten newest Reviews"
     def resolve
-      return Review.order(:created_at).last(10)
+      return Review.order(created_at: :desc).first(10)
     rescue
       raise GraphQL::ExecutionError.new "Unable to fetch ten newest Reviews"
     end
