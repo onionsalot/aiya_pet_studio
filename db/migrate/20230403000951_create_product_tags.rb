bb3bf8e2d86570b8d3e135e65b4ec5161c7d653e
onionsalot <onionsinmypants@gmail.com>
2023-04-02 20:20:01 -0400

    Adds product_tag model

---
+++ db/migrate/20230403000951_create_product_tags.rb
@@ -0,0 +1,10 @@
+class CreateProductTags < ActiveRecord::Migration[7.0]
+  def change
+    create_table :product_tags do |t|
+      t.references :product, null: false, foreign_key: true
+      t.references :tag, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_25_200401) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_03_000951) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_25_200401) do
     t.bigint "user_id"
     t.index ["email"], name: "index_potential_users_on_email", unique: true
     t.index ["user_id"], name: "index_potential_users_on_user_id"
+  end
+
+  create_table "product_tags", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_product_tags_on_product_id"
+    t.index ["tag_id"], name: "index_product_tags_on_tag_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -146,6 +155,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_25_200401) do
   add_foreign_key "etsy_etsy_reviews", "reviews"
   add_foreign_key "featured_products", "products"
   add_foreign_key "potential_users", "users"
+  add_foreign_key "product_tags", "products"
+  add_foreign_key "product_tags", "tags"
   add_foreign_key "reviews", "products"
   add_foreign_key "reviews", "users"
 end
