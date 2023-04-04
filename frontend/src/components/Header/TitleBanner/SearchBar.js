11901442c09cfa56b8702ec2669990213a2f574d
onionsalot <onionsinmypants@gmail.com>
2023-04-03 21:49:12 -0400

    Updates search bar component to navigate to a search page

--- frontend/src/components/Header/TitleBanner/SearchBar.js
+++ frontend/src/components/Header/TitleBanner/SearchBar.js
@@ -1,14 +1,25 @@
 import { BiSearch } from "react-icons/bi"
+import { useRef } from "react"
+import { useNavigate } from "react-router-dom"
 
 const SearchBar = () => {
   const searchIcon = <BiSearch className="text-2xl" />
-  
+  const formRef = useRef()
+  const navigate = useNavigate();
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    const formData = new FormData(formRef.current)
+    const data = Object.fromEntries(formData)
+    navigate(`/app/search?query=${encodeURIComponent(data.searchTerm)}`)
+  }
+
   return (
-    <div className="flex justify-between border-2 rounded-md border-gray-600 mx-2 px-1 hover:text-indigo-400/80 hover:border-indigo-400/80 ease-in duration-150">
-      <input type="text" className="bg-transparent focus:outline-none w-full"></input>
-      <button>{searchIcon}</button>
-    </div>
+    <form ref={formRef} onSubmit={handleSearch} className="flex justify-between border-2 rounded-md border-gray-600 mx-2 px-1 hover:text-indigo-400/80 hover:border-indigo-400/80 ease-in duration-150">
+      <input type="text" name="searchTerm" className="bg-transparent focus:outline-none w-full"></input>
+      <button type="submit">{searchIcon}</button>
+    </form>
   )
 }
 
-export default SearchBar;
+export default SearchBar
