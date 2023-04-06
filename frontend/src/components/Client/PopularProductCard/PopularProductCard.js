05b33a04499aaf8a7d1fa7cfc985a65b4e0e160e
eerian <eerian00@gmail.com>
2023-04-05 22:03:43 -0400

    fixes responsive design and styles

--- frontend/src/components/Client/PopularProductCard/PopularProductCard.js
+++ frontend/src/components/Client/PopularProductCard/PopularProductCard.js
@@ -4,7 +4,7 @@ import PopularProductCardSkeleton from "../Skeletons/PopularProductCardSkeleton/
 import TextSection from "../TextSection/TextSection";
 
 const PopularProductCard = () => {
-    const { data, status } = usePopularProduct();
+    const { data, status} = usePopularProduct();
     const popularProduct = data?.data?.data.popularProduct
     const sampleImage = "https://i.etsystatic.com/24762991/r/il/9f62c0/3430721950/il_794xN.3430721950_4c7c.jpg"
     if (status === "error") return <h1>Something went wrong!</h1>
@@ -12,20 +12,28 @@ const PopularProductCard = () => {
     
     return (
         <div>
-            <div className="mx-auto max-w-6xl px-6 md:px-2">
+            <div className="mx-auto mb-6 max-w-6xl px-6 md:px-2">
                 <div className="flex flex-col md:flex-row md:h-96">
                     <div className="w-full md:w-96 mr-10">
-                        <img src={sampleImage} className="w-96 h-96 object-cover" />
+                        <img src={sampleImage} className="w-full object-cover md:h-96" />
                     </div>
                     <div className="flex flex-col h-80 justify-between md:h-96">
                         <div className="w-full mt-2">
-                            <div className="h-14">
+                            <div className="h-12">
                                 <span className="text-2xl pt-3">{popularProduct.name}</span>
                             </div>
                             <TextSection title="Price" content={`$${popularProduct.price}`} />
-                            <TextSection title="Description" content={popularProduct.description} />
+                            <div className="w-full md:w-96 mt-2">
+                                <TextSection title="Description" content={`${popularProduct.description}
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                    culpa qui officia deserunt mollit anim id est laborum.`} />
+                            </div>
                         </div>
-                        <div className="flex flex-col items-start mb-4">
+                        <div>
                             <AddToCartButton />
                         </div>
                     </div>
--- frontend/src/components/Client/Skeletons/PopularProductCardSkeleton/PopularProductCardSkeleton.js
+++ frontend/src/components/Client/Skeletons/PopularProductCardSkeleton/PopularProductCardSkeleton.js
@@ -3,33 +3,35 @@ import 'react-loading-skeleton/dist/skeleton.css'
 
 const PopularProductCardSkeleton = () => {
     return (
-            <div className="mx-auto mb-8 max-w-6xl px-6 md:px-2">
-                <div className="flex flex-col md:flex-row">
-                    <div className="mb-2 w-96 mr-10 h-96">
-                        <Skeleton height="100%" baseColor='#ffeaf1' highlightColor='#fff5f7'/>
-                    </div>
-                    <div className="flex flex-col w-96 h-80 justify-between md:h-96">
-                        <div className="w-full mt-2">
-                            <div className="w-full h-14">
-                                <Skeleton height={35} baseColor='#ffeaf1' highlightColor='#fff5f7' />
-                            </div>
-                            <div>
-                                <div className="h-14">
-                                    <Skeleton width="20%" height={30} baseColor='#ffeaf1' highlightColor='#fff5f7'/>
-                                    <Skeleton width="20%" height={30} baseColor='#ffeaf1' highlightColor='#fff5f7'/>
-                                </div>
-                                <div className='mt-6'>
-                                    <Skeleton width="30%" height={30} baseColor='#ffeaf1' highlightColor='#fff5f7'/>
-                                    <Skeleton height={120} baseColor='#ffeaf1' highlightColor='#fff5f7'/>        
-                                </div>
-                            </div>
+        <div className="mx-auto mb-6 max-w-6xl px-6 md:px-2">
+            <div className="flex flex-col md:flex-row">
+                <div className="mb-2 w-full mr-10 h-96 md:w-96">
+                    <Skeleton height="100%" baseColor='#ffeaf1' highlightColor='#fff5f7'/>
+                </div>
+                <div className="flex flex-col justify-between md:w-96 md:h-96">
+                    <div className="w-full mt-2">
+                        <div className="w-full h-14">
+                            <Skeleton height={35} baseColor='#ffeaf1' highlightColor='#fff5f7' />
                         </div>
                         <div>
-                            <Skeleton height={40} width="35%" baseColor='#ffeaf1' highlightColor='#fff5f7'/>
+                            <div className="h-14">
+                                <Skeleton width="20%" height={25} baseColor='#ffeaf1' highlightColor='#fff5f7'/>
+                                <Skeleton width="20%" height={25} baseColor='#ffeaf1' highlightColor='#fff5f7'/>
+                            </div>
+                            <div className='mt-3'>
+                                <Skeleton width="30%" height={30} baseColor='#ffeaf1' highlightColor='#fff5f7'/>
+                                <div>
+                                    <Skeleton width="100%" height={120} baseColor='#ffeaf1' highlightColor='#fff5f7' />
+                                </div>
+                            </div>
                         </div>
+                    </div>
+                    <div>
+                        <Skeleton height={35} width="35%" baseColor='#ffeaf1' highlightColor='#fff5f7'/>
                     </div>
                 </div>
             </div>
+        </div>
     )
 }
 
