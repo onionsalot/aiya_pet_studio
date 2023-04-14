5a7d1cc718b3e10a6fc96724d3d61a52d03ca3ec
onionsalot <onionsinmypants@gmail.com>
2023-04-14 10:40:28 -0400

    Centers the dropdown text

--- frontend/src/components/Client/Dropdown/Dropdown.js
+++ frontend/src/components/Client/Dropdown/Dropdown.js
@@ -7,7 +7,7 @@ const Dropdown = ({prefix, sortOptions = [], handleOptions, defaultValue}) => {
     <div className="relative form-input py-0 w-48 m-4 md:my-0 border-gray-600 px-1 hover:border-indigo-400/80 ease-in duration-150 bg-transparent">
       <span className="absolute left-2 bottom-[10px] z-0 pointer-events-none">{prefix && `${prefix}:`} </span>
       <select
-        className="bg-transparent focus:outline-none w-full pl-8 font-medium py-2"
+        className="bg-transparent focus:outline-none w-full text-center font-medium py-2"
         required
         onChange={handleOptions}
         defaultValue={defaultValue}
