6d2b38a7e398e247639ef3d632f9401af5ed5aa6
Trong Nguyen <onionsinmypants@gmail.com>
2023-04-15 17:01:00 -0400

    Aps 60 design frontend search page (#41)
    * Renames Featured product card and skeleton to be more reusable
    
    * Adds new product queries for search purposes
    
    * Updates search bar component to navigate to a search page
    
    * Adds routes and content for search page
    
    * Resets form once search is used
    
    * Adds new universal tailwind property
    
    * Adds reuseable dropdown component and sets up groundwork for sorting feature
    
    * Adds skeleton loader
    
    * Renames useSearchedProducts to useSearchProducts for name consistency
    
    * Handles case where no products are found
    
    * Centers and changes not found message
    
    * Centers the dropdown text
    
    * Adds frontend check for empty search bar
    
    * Adds backend check for empty search phrases
    
    * creates new hooks to determine when an error occurs
    
    * Bubbles up the GQL error message to display to the FE
    
    * Removes leftover console log

--- app/graphql/queries/search_products.rb
+++ app/graphql/queries/search_products.rb
@@ -6,9 +6,11 @@ module Queries
     argument :search_term, String, required: true
 
     def resolve(search_term:)
+      raise GraphQL::ExecutionError.new("Search term cannot be blank") if search_term.blank?
+
       Product.left_outer_joins(:tags).where("products.name ILIKE ? OR tags.name ILIKE ?", "%#{search_term}%", "%#{search_term}%").distinct
-    rescue
-      raise GraphQL::ExecutionError.new "Unable to find any products"
+    rescue => e
+      raise GraphQL::ExecutionError.new(e.message)
     end
   end
 end
---
+++ frontend/src/pages/Client/SearchPage/SearchPage.js
@@ -0,0 +1,81 @@
+import { useLocation, useNavigate } from "react-router-dom"
+import { useSearchProducts } from "../../../hooks/product-hooks"
+import SearchSection from "../../../components/Client/SearchSection/SearchSection"
+import Dropdown from "../../../components/Client/Dropdown/Dropdown"
+import { useState, useEffect, useMemo } from "react"
+import ProductCardSkeleton from "../../../components/Client/Skeletons/ProductCardSkeleton/ProductCardSkeleton"
+
+const SearchPage = () => {
+  const searchParams = new URLSearchParams(useLocation().search)
+  const searchTerm = searchParams.get("query")
+  const sortQuery = searchParams.get("sort") || "relevant"
+  const { status, data: productData, error } = useSearchProducts({ searchTerm })
+  const foundProducts = productData?.data?.data?.searchProducts
+  const [sortBy, setSortBy] = useState(sortQuery)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    setSortBy(sortQuery)
+  }, [sortQuery])
+
+  const content = useMemo(
+    () => {
+      if (status === 'error') return <h1>{error.message || "Something went wrong!"}</h1>
+      if (status === 'loading') {
+        return Array(16)
+        .fill()
+        .map((_, i) => (
+          <ProductCardSkeleton key={i} />
+          )
+        )
+      }
+      return <SearchSection foundProducts={foundProducts} />
+    }, [status]
+  )
+
+  const sortOptions = [
+    {
+      value: "relevant",
+      display: 'Relevance'
+    },
+    {
+      value: "lowestPrice",
+      display: 'Lowest Price'
+    },
+    {
+      value: "highestPrice",
+      display: 'Highest Price'
+    },
+    {
+      value: "topReviewed",
+      display: 'Top Reviewed'
+    },
+    {
+      value: "mostRecent",
+      display: 'Most Recent'
+    }
+  ]
+
+  const handleOptions = (e) => {
+    setSortBy(e.target.value)
+
+    const updatedSearchParams = new URLSearchParams(searchParams);
+    updatedSearchParams.set("sort", e.target.value);
+    navigate(`/app/search?${updatedSearchParams.toString()}`, { replace: true });
+  }
+
+  return (
+    <div className="flex flex-col">
+      <div className="align-center-max">
+        <div className="ml-auto">
+          <Dropdown prefix={"Sort"} sortOptions={sortOptions} handleOptions={handleOptions} defaultValue={sortQuery} />
+        </div>
+      </div>
+      <div className="align-center-max">
+        {content}
+      </div>
+    </div>
+  )
+}
+ 
+export default SearchPage
