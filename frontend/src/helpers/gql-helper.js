7d9c912aa96d0cbd7ba482a8a7794362e4d11b63
onionsalot <onionsinmypants@gmail.com>
2023-04-17 21:49:36 -0400

    Updates url for api

--- frontend/src/helpers/gql-helper.js
+++ frontend/src/helpers/gql-helper.js
@@ -1,6 +1,6 @@
 import axios from "../lib/axios"
 
-const API_URL = process.env.REACT_APP_API_URL + "/graphql"
+const API_URL = process.env.REACT_APP_API_URL + "graphql"
 
 export const gqlHelper = async (query, input = null) => {
   if (input !== null) query.variables = input
--- frontend/src/helpers/users-api.js
+++ frontend/src/helpers/users-api.js
@@ -1,6 +1,6 @@
 import axios from "../lib/axios"
 
-const BASE_URL = process.env.REACT_APP_API_URL + "/auth/"
+const BASE_URL = process.env.REACT_APP_API_URL + "auth/"
 
 export function getCurrentUser() {
   if (localStorage.getItem('isLoggedIn') !== 'true') return null
