1aec37602bf37bfa94ee8c286d49e666004d3045
Angel Zhou <angel.zhou1016@gmail.com>
2023-04-28 18:32:29 -0400

    changes <Link> tag to <a> tag to allow routing to an external url

--- frontend/src/components/Client/Social/Instagram.js
+++ frontend/src/components/Client/Social/Instagram.js
@@ -1,9 +1,8 @@
 import { BsInstagram } from "react-icons/bs"
-import { Link } from "react-router-dom";
 
 const Instagram = () => {
   return (
-    <Link className="flex mb-2 mt-4" to="https://www.instagram.com/aiyapetstudio/">Follow us <BsInstagram className="ml-2 text-xl" /></Link>
+    <a className="flex mb-2 mt-4" href="https://www.instagram.com/aiyapetstudio/" target="_blank">Follow us <BsInstagram className="ml-2 text-xl" /></a>
   );
 }
  
